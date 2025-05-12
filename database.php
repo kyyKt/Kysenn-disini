89a9ee0947985f38219f6ccd33aaad6f01fc4d4e
kyyKt <145518026+kyyKt@users.noreply.github.com>
2025-05-12 22:16:53 +0700

    database.php
    tgs8

---
+++ database.php
@@ -0,0 +1,16 @@
+?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "db_film";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        if ($this->conn->connect_error) {
+            die("Koneksi gagal: " . $this->conn->connect_error);
+        }
+        return $this->conn;
+    }
+}
